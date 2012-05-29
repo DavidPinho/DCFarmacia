667d60ff6db4632c6826f26088c25555360a7fe7
Caio Lima <ticaiolima@gmail.com>
2012-05-28 21:09:59 -0300

    cadastro remedio implementado

--- src/DCFarmacia.c
+++ src/DCFarmacia.c
@@ -11,11 +11,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Produto.h"
+#include <string.h>
+
+void cadastraRemedio(ProdutoList *l);
 
 int main(void) {
-	int n=1;
+    ProdutoList l;
+    initList(&l);
     
-	teste(&n);
-	printf("%d",n); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+    printf("Bem vindo ao sistema DCFarmacia! Aqui você terá controle sobre o que está  acontecendo com seu estoque farmaceutico\n\n\n-----------------------------------------------------\n\n");
+    int op;
+    do{
+        printf("Menu\n\n1 - Cadastro Usuario\n2 - Editar Usuario\n3 - Remover Usuario\n4 - Cadastrar Remedio\n5 - Editar Remedio\n6 - Remover Remedio\n0 - Sair");
+        printf("\n\nOp: "); scanf("%d",&op);
+        switch (op) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                cadastraRemedio(&l);
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 0:
+                printf("\n\nObrigado pela preferência!");
+                break;
+                
+        }
+    }while(op!=0);
+    
+    
+    return EXIT_SUCCESS;
+}
+
+void cadastraRemedio(ProdutoList *l){
+    Produto p;
+    printf("\n\n\nCadastro de remédio\n--------------------------------------------------------------");
+    int id;
+    printf("\nID: "); scanf("%d%*c",&id);
+    p.idProduto=id;
+    
+
+    printf("Descricao: "); gets(p.desc);
+    
+    float preco;
+    printf("Preco: "); scanf("%f",&preco);
+    p.preco=preco;
+    
+    int qtd;
+    do{
+        printf("Qtd: "); scanf("%d",&qtd);
+    }while (qtd<1);
+    
+    int i;
+    for(i=0;i<qtd;i++){
+        Produto pNovo=p;
+        pNovo.idProduto+=i;
+        if(add(l, pNovo))
+            printf("\n\nProduto cadastrado com sucesso!");
+        else {
+            printf("\n\nErro no cadastro do produto!");
+        }
+    }
+    
 }
